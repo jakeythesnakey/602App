59d20fc9d6e394c9889bfe3ad20573b8ba3cb1cc
Jake <j.kampkes@gmail.com>
2019-05-15 07:53:23 +1200

    Test home page added

---
+++ app/src/main/java/com/example/a602app/QuickSelect.kt
@@ -0,0 +1,12 @@
+package com.example.a602app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class QuickSelect : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quick_select)
+    }
+}
